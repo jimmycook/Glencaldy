b7e40b5856414f3e32f0e71c8c236cbf8e62674c
Jimmy Cook <jimmy.cook.1993@gmail.com>
2015-02-21 02:21:08 +0000

    first menu in place

--- src/glencaldy/LearningCenter.java
+++ src/glencaldy/LearningCenter.java
@@ -11,38 +11,141 @@ public class LearningCenter {
 	
 	private ArrayList<User> allUsers = new ArrayList<User>();
 	private ArrayList<Stock> allStock = new ArrayList<Stock>();
+	private ArrayList<LoginRecord> allLogins = new ArrayList<LoginRecord>();
 	private InputStreamReader converter = new InputStreamReader(System.in);
+	User activeUser = null;
+	LoginRecord activeLogin = null;
 	BufferedReader in = new BufferedReader(converter);
 	
 	LearningCenter(){
-		LoginRecord loginRecord = null;
-		System.out.println("Controller starting\n\n");
-		System.out.println("testing");
+		boolean logout = false;
 		
-		this.populateUsers();
-		while(loginRecord == null){
-			loginRecord = login();
+		System.out.println("Controller starting\n\n");
+		
+		populateUsers();
+		
+		welcomeMessage();
+		
+		while(activeUser == null){
+			activeUser = login();
 		}
+		
+		activeLogin = new LoginRecord(activeUser.getUserID());
+		allLogins.add(activeLogin);
+		while(!logout)
+		{
+			getMenu();
+		}
+		
+		logout();
+	}
+	
+	
+	private void logout() {
+		
+	}
+
+
+	/*
+	 * Gets the menu for the correct type of user 
+	 */
+	public void getMenu(){
+		String userType = activeUser.getClass().getName();
+		
+		if(userType.equals("glencaldy.CasualUser")){
+			casualMenu();
+		}
+		else if(userType.equals("glencaldy.Administrator")){
+			adminMenu();
+		}
+		else{
+			fullMenu();
+		}
+	}
+	
+	private void fullMenu() {
+		int input = null;
+		
+		do{
+			System.out.println("Full Menu");
+			System.out.println("----------------");
+			System.out.println("1. View Stock Catalogue");
+			System.out.println("2. Change Password");
+			System.out.println("3. Request Reservation");
+			System.out.println("0. Logout");
+			System.out.println("\nEnter an option");
+			
+			try{
+				String tempInput = in.readLine();
+			}
+			catch (IOException e){
+					System.err.println("Error : " + e);
+			}
+			
+			
+			switch (input){ 
+			case 1:
+				viewCatalogue();
+				break;
+			case 2:
+				changePassword();
+				break;
+			case 3:
+				requestReservation();
+				break;
+			}
+		}
+		while(input != 0);
 		
 		
 	}
-	
+
+
+	private void requestReservation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void changePassword() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void viewCatalogue() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void until(boolean equals) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void adminMenu() {
+		
+	}
+
+
+	private void casualMenu() {
+		
+	}
+
+
 	/*
-	 * 
 	 * Login - asks the user for a username and password until they are successfully logged in to the system
 	 * 
-	 * @return LoginRecord made if login was successful
+	 * @return User made if login was successful, otherwise returns null
 	 */
-	public LoginRecord login(){
-		boolean loggedIn = false;
-		boolean found = false;
+	public User login(){
 		User curUser = null;
 		String username = null;
 		String password = null;
 		Iterator<User> userIt = allUsers.iterator();
-		
-		found = false;
-		
+				
 		/*
 		 * get username and password from the user
 		 */
@@ -70,30 +173,27 @@ public class LearningCenter {
 			curUser = userIt.next();
 			
 			if(username.equals(curUser.getUsername())){
-				found = true;
 				if(password.equals(curUser.getPassword())){
-					
-					System.out.println("Logged in as " + curUser.getUsername());
-					return new LoginRecord(curUser.getUserID());
-					
+					System.out.println("\n\nLogging in as '" + curUser.getUsername() + "'\n\n");
+					return curUser;
 				}
 				else{
-					
-					System.out.println("Login failed - incorrect password\n\n");
-					
+					System.out.println("Login failed - incorrect password\n\n");	
 				}
-			}
-			else{
-				
-				System.out.println("Login failed - user not found\n\n");
-			
 			}
 		}
 		
+		System.out.println("Login failed - user not found\n\n");
+
 		return null;
 		
 	}
 	
+	public void welcomeMessage(){
+		System.out.println("--------------------------------------\n");
+		System.out.println("Welcome to Glencaldy Learning Center\n");
+		System.out.println("--------------------------------------\n");
+	}
 	
 	public void populateUsers(){
 				
